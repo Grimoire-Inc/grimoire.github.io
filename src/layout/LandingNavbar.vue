e850e749878dd150635dc14354ab69729b122e29
alang <arthur.lang@epitech.eu>
2021-05-21 11:24:10 +0200

    fix disepearing files

---
+++ src/layout/LandingNavbar.vue
@@ -0,0 +1,82 @@
+<template>
+  <navbar
+    position="fixed"
+    type="primary"
+    :transparent="transparent"
+    :color-on-scroll="colorOnScroll"
+    menu-classes="ml-auto"
+  >
+
+    <template slot-scope="{ toggle, isToggled }">
+      <router-link v-popover:popover1 class="navbar-brand" to="/">
+        Grimoire
+      </router-link>
+    </template>
+
+
+    <template slot="navbar-menu">
+
+      <!-- Liens des réseaux sociaux -->
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Follow us on Twitter"
+          data-placement="bottom"
+          href=""
+        >
+          <i class="fab fa-twitter"></i>
+          <p class="d-lg-none d-xl-none">Twitter</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Like us on Facebook"
+          data-placement="bottom"
+          href="https://www.facebook.com/GrimoireFr"
+          target="_blank"
+        >
+          <i class="fab fa-facebook-square"></i>
+          <p class="d-lg-none d-xl-none">Facebook</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Follow us on Instagram"
+          data-placement="bottom"
+          href="https://www.instagram.com/projet_grimoire"
+          target="_blank"
+        >
+          <i class="fab fa-instagram"></i>
+          <p class="d-lg-none d-xl-none">Instagram</p>
+        </a>
+      </li>
+
+    </template>
+  </navbar>
+</template>
+
+<script>
+import { DropDown, NavbarToggleButton, Navbar, NavLink } from '@/components';
+import { Popover } from 'element-ui';
+export default {
+  name: 'main-navbar',
+  props: {
+    transparent: Boolean,
+    colorOnScroll: Number
+  },
+  components: {
+    DropDown,
+    Navbar,
+    NavbarToggleButton,
+    NavLink,
+    [Popover.name]: Popover
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicPath: '/grimoire.github.io/',
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production'
+  }
+};
